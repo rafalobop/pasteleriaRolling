5d478348e8ae35bf6ec3b2ea97f5493b3fe59030
rafalobop <r.loboplaza@hotmail.com>
2021-03-27 08:47:29 -0300

    terminando formulario de registro

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// let signinButton = document.querySelector('#sign-in-button');
+// let signupButton = document.querySelector('#sign-up-button');
+// let container = document.querySelector('.container');
+
+// signupButton.addEventListener('click', () => {
+//   container.classList.add('sign-up-mode');
+// });
+// signinButton.addEventListener('click', () => {
+//   container.classList.remove('sign-up-mode');
+// });
