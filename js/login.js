1f4d9f33eff88707eeb9f17bd3ee82a4e3ee13d8
rafalobop <r.loboplaza@hotmail.com>
2021-04-08 12:48:07 -0300

    validacion de registro lista

--- js/login.js
+++ js/login.js
@@ -1,59 +1,4 @@
-// let form = document.querySelector('.form');
-// let user = document.querySelector('#user');
-// let pass = document.querySelector('#pass');
-
-// form.addEventListener('submit', enviarForm);
-
-// class Usuario {
-//   constructor(id, usuario, password, activo = true) {
-//     this.id = id;
-//     this.usuario = usuario;
-//     this.password = password;
-//     this.activo = activo;
-//   }
-// }
-
-// let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
-
-// // creamos una funcion para generar un id automatico
-// function idRandom() {
-//   if (usuarios.length > 0) {
-//     return usuarios[usuarios.length - 1].id + Math.round(Math.random() * 100);
-//   } else {
-//     return Math.round(Math.random() * 100);
-//   }
-// }
-
-// function enviarForm(e) {
-//   e.preventDefault();
-
-//   let id = idRandom();
-//   let usuario = document.querySelector('#user').value;
-//   let password = document.querySelector('#pass').value;
-
-//   let validar = usuarios.find(function (user) {
-//     return user.usuario === usuario;
-//   });
-
-//   if (validar !== undefined) {
-//     alert('Usuario existente. Intenta con otro email');
-
-//     form.reset();
-//     // usuario.focus();
-
-//     return;
-//   }
-
-//   let newUser = new Usuario(id, usuario, password);
-
-//   usuarios.push(newUser);
-
-//   localStorage.setItem('usuarios', JSON.stringify(usuarios));
-
-//   form.reset();
-//   alert('Registro exitoso');
-// }
-
+//animacion de aparecer y desaparecer formulario
 let btnReg = document.querySelector('#btn_reg');
 let btnLogin = document.querySelector('#btn_login');
 let formLog = document.querySelector('#form_log');
@@ -103,3 +48,72 @@ btnReg.addEventListener('click', function (e) {
     formReg.classList.add('show', 'animate__animated', 'animate__fadeInUp');
   }
 });
+
+//validacion de registro
+//creamos el usuario
+
+class Usuario {
+  constructor(idReg, usuario, password, passRep, email, activo = true) {
+    this.idReg = idReg;
+    this.usuario = usuario;
+    this.password = password;
+    this.passRep = passRep;
+    this.email = email;
+    this.activo = activo;
+  }
+}
+
+let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+// creamos una funcion para generar un id automatico
+function idRandom() {
+  if (usuarios.length > 0) {
+    return (
+      usuarios[usuarios.length - 1].idReg + Math.round(Math.random() * 100)
+    );
+  } else {
+    return Math.round(Math.random() * 100);
+  }
+}
+
+let formularioReg = document.querySelector('#form_r');
+formularioReg.addEventListener('submit', validarRegistro);
+
+function validarRegistro(e) {
+  e.preventDefault();
+
+  let idReg = idRandom();
+  let usuarioReg = document.querySelector('#user_r').value;
+  let passwordReg = document.querySelector('#pass_r').value;
+  let passRep = document.querySelector('#pass_rep').value;
+  let emailReg = document.querySelector('#email_r').value;
+
+  let validar = usuarios.find(function (user) {
+    return user.email === email;
+  });
+
+  if (validar !== undefined) {
+    alert('Usuario existente. Intenta con otro email');
+
+    formularioReg.reset();
+
+    return;
+  }
+  if (passwordReg !== passRep) {
+    let divError = document.querySelector('.error');
+    divError.textContent = 'Las contraseñas no coinciden';
+    setTimeout(() => {
+      divError.remove();
+    }, 1500);
+    formularioReg.reset();
+  }
+
+  let newUser = new Usuario(idReg, usuarioReg, passwordReg, passRep, emailReg);
+
+  usuarios.push(newUser);
+
+  localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+  formularioReg.reset();
+  alert('Registro exitoso');
+}
