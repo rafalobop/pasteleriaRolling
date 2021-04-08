5e15604e6c14941e89d8e6c426b4fff259b40b25
rafalobop <r.loboplaza@hotmail.com>
2021-04-08 16:35:15 -0300

    boton ir arriba agregado a todas las paginas

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,20 @@
+//boton desplazar suave hacia arriba
+window.onscroll = function () {
+  let boton = document.querySelector('.btn_goup');
+  if (document.documentElement.scrollTop < 200) {
+    boton.classList.add('hide');
+  } else {
+    boton.classList.remove('hide');
+    boton.classList.add('show');
+  }
+  document.querySelector('.btn_goup').addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  });
+};
+
 class Producto {
   constructor(codigo, nombre, precio, imagen, stock) {
     this.codigo = codigo;
